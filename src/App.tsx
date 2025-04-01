d1cc16ee08bad5461fe27097a1a2bda0772ef3c8
ZouFitahiana <fitahianaherizo10@gmail.com>
2025-04-01 11:13:05 +0300

    config tailwind css

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,9 @@ import './App.css'
 function App() {
   return (
     <>
-      
+      <footer className="text-center mt-4 text-2xl text-red-500">
+        &copy; 2025 Herizo. All rights reserved.
+      </footer>
     </>
   )
 }
